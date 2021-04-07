4697e3f92be71c6b23db56b056a269d68053457e
Joshy Orndorff <admin@joshyorndorff.com>
2021-04-07 13:54:29 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+
+pub struct Number {
+    value: u64,
+}
+
+impl Number {
+    pub fn double(&self) -> u64 {
+        self.value * 2
+    }
+
+    pub fn tripple(&self) -> u64 {
+        self.value * 3
+    }
+}
